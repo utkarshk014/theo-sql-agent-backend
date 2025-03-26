21d642aae36550e350e96829c1e0b2d4f7ae3b15
utkarshk014 <utkarsh@instasell.in>
2025-03-26 15:04:43 +0530

    add allowed origins

--- app/main.py
+++ app/main.py
@@ -15,7 +15,7 @@ app = FastAPI(title="Theo SQL Agent",
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  
+    allow_origins=["https://theo-sql-agent-frontend.vercel.app/", "http://localhost:3000"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
